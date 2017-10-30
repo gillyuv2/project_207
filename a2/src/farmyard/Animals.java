4c1202140d2d18604afccee452a082b428e43a31
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2017-10-30 05:31:08 -0400

    Added the changes in the Animals file and made note under Step 23 in notes.txt.

--- a2/src/farmyard/Animals.java
+++ a2/src/farmyard/Animals.java
@@ -6,160 +6,163 @@ import java.util.ArrayList;
 
 public class Animals extends FarmMethods {
 
-    private ArrayList<FarmMethods> collector = new ArrayList<FarmMethods>();
-    private String manure;
-    private FarmMethods target = null;
-    private GraphicsContext g;
+  private ArrayList<FarmMethods> collector = new ArrayList<FarmMethods>();
+  private String manure;
+  private FarmMethods target = null;
+  private GraphicsContext g;
 
+  public Animals() {}
 
+  void setManure(String newManure) {
+    this.manure = newManure;
+  }
 
+  public void setTarget(FarmMethods newTarget) {
+    this.target = newTarget;
+  }
 
-    public Animals(){
+  String reverseAppearance() {
+    String reverse = "";
+    for (int i = this.getAppearance().length() - 1; i >= 0; i--) {
+      switch (this.getAppearance().charAt(i)) {
+        case '\\':
+          reverse += '/';
+          break;
+
+        case '/':
+          reverse += '\\';
+          break;
+        case ')':
+          reverse += '(';
+          break;
+        case '(':
+          reverse += ')';
+          break;
+        case '>':
+          reverse += '<';
+          break;
+        case '<':
+          reverse += '>';
+          break;
+        case '}':
+          reverse += '{';
+          break;
+        case '{':
+          reverse += '}';
+          break;
+        case '[':
+          reverse += ']';
+          break;
+        case ']':
+          reverse += '[';
+          break;
+        default:
+          reverse += this.getAppearance().charAt(i);
+          break;
+      }
+    }
+    return reverse;
+  }
+
+  void turnAround() {
+    this.setAppearance(this.reverseAppearance());
+    if (this.getX() <= 0) {
+      this.setLocation(1, this.getY());
+    } else if (this.getX() >= 1024) {
+      this.setLocation(1023, this.getY());
+    }
+    if (this.getY() <= 0) {
+      this.setLocation(this.getX(), 1);
+    } else if (this.getY() >= 720) {
+      this.setLocation(this.getX(), 719);
+    }
+  }
+
+  private boolean digest() {
+
+    AnimalManure newManure = new AnimalManure(this.getX(), this.getY());
+    newManure.setAppearance(this.manure);
+    return true;
+  }
+
+  static Egg eggLocator() {
+    if (Farm.eggList.size() != 0) {
+      return Farm.eggList.get(0);
+    }
+    return null;
+  }
+
+  private static AnimalFood foodLocator() {
+    if (Farm.foodList.size() != 0) {
+      return Farm.foodList.get(0);
+    }
+    return null;
+  }
+
+  static AnimalManure manureLocator() {
+    if (Farm.manureList.size() != 0) {
+      return Farm.manureList.get(0);
+    }
+    return null;
+  }
+
+  public void move() {
+
+    double d = Math.random();
+    if (target == null) {
+      target = Animals.foodLocator();
     }
 
-    void setManure(String newManure){
-        this.manure = newManure;
-    }
+    if (target != null) {
 
-    public void setTarget(FarmMethods newTarget){
-        this.target = newTarget;
-    }
+      if (this.getX() == target.getX() && this.getY() == target.getY()) {
 
-    String reverseAppearance() {
-        String reverse = "";
-        for (int i = this.getAppearance().length() - 1; i >= 0; i--) {
-            switch (this.getAppearance().charAt(i)) {
-                case '\\':
-                    reverse += '/';
-                    break;
+        this.collector.add(target);
 
-                case '/':
-                    reverse += '\\';
-                    break;
-                case ')':
-                    reverse += '(';
-                    break;
-                case '(':
-                    reverse += ')';
-                    break;
-                case '>':
-                    reverse += '<';
-                    break;
-                case '<':
-                    reverse += '>';
-                    break;
-                case '}':
-                    reverse += '{';
-                    break;
-                case '{':
-                    reverse += '}';
-                    break;
-                case '[':
-                    reverse += ']';
-                    break;
-                case ']':
-                    reverse += '[';
-                    break;
-                default:
-                    reverse += this.getAppearance().charAt(i);
-                    break;
-            }
-        }
-        return reverse;
-    }
+        Farm.track1.add(target);
+        Farm.foodList.remove(target);
 
-    void turnAround() {
-        this.setAppearance(this.reverseAppearance());
-    }
+        target = null;
 
-    boolean digest() {
-//    System.out.println("Pig stink");
+      } else {
 
-        AnimalManure newManure = new AnimalManure(this.getX(), this.getY());
-        newManure.setAppearance(this.manure);
-        return true;
-    }
-
-    static Egg eggLocator() {
-        if (Farm.eggList.size() != 0 ){
-            return Farm.eggList.get(0);
-        }
-        return null;
-    }
-    static AnimalFood foodLocator() {
-        if (Farm.foodList.size() != 0){
-            return  Farm.foodList.get(0);
-        }
-        return null;
-    }
-
-    public static AnimalManure manureLocator() {
-        if (Farm.manureList.size() != 0 ){
-            return Farm.manureList.get(0);
-
-        }
-        return null;
-    }
-
-    public void moveAnimal() {
-        double d = Math.random();
-        if (target == null) {
-            target = Animals.eggLocator();
-            System.out.println(target);
-        }
-
-        if (target != null) {
-
-            // Am I on an egg?
-            if (this.getX() == target.getX() && this.getY() == target.getY()) {
-
-                this.collector.add(target);
-                Farm.myFarmAnimals.remove(target);
-                target = null;
-
-                if (collector.size() % 12 == 0) {
-                    this.collector.clear();
-                    this.draw(g);
-                }
-
-            } else {
-
-                // move toward the egg
-                if (this.getY() < target.getY()) {
-                    this.setLocation(this.getX(), this.getY() + 1);
-                } else if (this.getY() > target.getY()) {
-                    this.setLocation(this.getX(), this.getY() - 1);
-                } else {
-                    this.setLocation(this.getX(), this.getY());
-                }
-
-                if (this.getX() < target.getX()) {
-                    this.setLocation(this.getX() + 1, this.getY());
-                } else if (this.getX() > target.getX()) {
-                    this.setLocation(this.getX() - 1, this.getY());
-                } else {
-                    this.setLocation(this.getX(), this.getY());
-                }
-            }
+        // move toward the egg
+        if (this.getY() < target.getY()) {
+          this.setLocation(this.getX(), this.getY() + 1);
+        } else if (this.getY() > target.getY()) {
+          this.setLocation(this.getX(), this.getY() - 1);
         } else {
-            // no egg to pick up
-            // Move one spot to the right or left.
-
-            if (d < 0.25) {
-                this.setLocation(this.getX(), this.getY() + 1);
-            } else if (0.25 < d && d < 0.5) {
-                this.setLocation(this.getX(), this.getY() - 1);
-            } else if (0.5 < d && d < 0.75) {
-                this.setLocation(this.getX() + 1, this.getY());
-            } else {
-                this.setLocation(this.getX() - 1, this.getY());
-            }
+          this.setLocation(this.getX(), this.getY());
         }
-
-        double f = Math.random();
-        if (f < 0.3) {
-            turnAround();
+        if (this.getX() < target.getX()) {
+          this.setLocation(this.getX() + 1, this.getY());
+        } else if (this.getX() > target.getX()) {
+          this.setLocation(this.getX() - 1, this.getY());
+        } else {
+          this.setLocation(this.getX(), this.getY());
         }
+      }
+    } else {
 
+
+      if (d < 0.25) {
+        this.setLocation(this.getX(), this.getY() + 3);
+      } else if (0.25 < d && d < 0.5) {
+        this.setLocation(this.getX(), this.getY() - 3);
+      } else if (0.5 < d && d < 0.75) {
+        this.setLocation(this.getX() + 3, this.getY());
+      } else {
+        this.setLocation(this.getX() - 3, this.getY());
+      }
     }
+
+    if (this.collector.size() > 1) {
+      this.collector.clear();
+      digest();
+    }
+
+    if (Math.random() < 0.1) {
+      turnAround();
+    }
+  }
 }
