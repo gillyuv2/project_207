0c22416894f6bd04661bc258a8ca1fa7c6c117ec
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2017-10-30 00:55:02 -0400

    Saved the changes in Animals.java and the changes are saved in the Step 15 in notes.txt

--- a2/src/farmyard/Animals.java
+++ a2/src/farmyard/Animals.java
@@ -1,23 +1,31 @@
 package farmyard;
 
+import javafx.scene.canvas.GraphicsContext;
+
 import java.util.ArrayList;
 
 public class Animals extends FarmMethods {
 
-    private ArrayList<FarmMethods> belly = new ArrayList<FarmMethods>();
+    private ArrayList<FarmMethods> collector = new ArrayList<FarmMethods>();
     private String manure;
-    private int horizontal;
-    private int vertical;
     private FarmMethods target = null;
+    private GraphicsContext g;
+
+
+
+
     public Animals(){
     }
 
-    public void setManure(String newManure){
+    void setManure(String newManure){
         this.manure = newManure;
-
     }
 
-    public String reverseAppearance() {
+    public void setTarget(FarmMethods newTarget){
+        this.target = newTarget;
+    }
+
+    String reverseAppearance() {
         String reverse = "";
         for (int i = this.getAppearance().length() - 1; i >= 0; i--) {
             switch (this.getAppearance().charAt(i)) {
@@ -60,18 +68,98 @@ public class Animals extends FarmMethods {
         return reverse;
     }
 
-    public void turnAround() {
+    void turnAround() {
         this.setAppearance(this.reverseAppearance());
     }
 
-    public boolean digest() {
+    boolean digest() {
 //    System.out.println("Pig stink");
 
-        AnimalManure newManure = new AnimalManure();
+        AnimalManure newManure = new AnimalManure(this.getX(), this.getY());
         newManure.setAppearance(this.manure);
-        newManure.setLocation(this.getX(), this.getY());
-
-        Human.myFarmAnimals[this.getX()][this.getY()] = newManure;
         return true;
+    }
+
+    static Egg eggLocator() {
+        if (Farm.eggList.size() != 0 ){
+            return Farm.eggList.get(0);
+        }
+        return null;
+    }
+    static AnimalFood foodLocator() {
+        if (Farm.foodList.size() != 0){
+            return  Farm.foodList.get(0);
+        }
+        return null;
+    }
+
+    public static AnimalManure manureLocator() {
+        if (Farm.manureList.size() != 0 ){
+            return Farm.manureList.get(0);
+
+        }
+        return null;
+    }
+
+    public void moveAnimal() {
+        double d = Math.random();
+        if (target == null) {
+            target = Animals.eggLocator();
+            System.out.println(target);
+        }
+
+        if (target != null) {
+
+            // Am I on an egg?
+            if (this.getX() == target.getX() && this.getY() == target.getY()) {
+
+                this.collector.add(target);
+                Farm.myFarmAnimals.remove(target);
+                target = null;
+
+                if (collector.size() % 12 == 0) {
+                    this.collector.clear();
+                    this.draw(g);
+                }
+
+            } else {
+
+                // move toward the egg
+                if (this.getY() < target.getY()) {
+                    this.setLocation(this.getX(), this.getY() + 1);
+                } else if (this.getY() > target.getY()) {
+                    this.setLocation(this.getX(), this.getY() - 1);
+                } else {
+                    this.setLocation(this.getX(), this.getY());
+                }
+
+                if (this.getX() < target.getX()) {
+                    this.setLocation(this.getX() + 1, this.getY());
+                } else if (this.getX() > target.getX()) {
+                    this.setLocation(this.getX() - 1, this.getY());
+                } else {
+                    this.setLocation(this.getX(), this.getY());
+                }
+            }
+        } else {
+            // no egg to pick up
+            // Move one spot to the right or left.
+
+            if (d < 0.25) {
+                this.setLocation(this.getX(), this.getY() + 1);
+            } else if (0.25 < d && d < 0.5) {
+                this.setLocation(this.getX(), this.getY() - 1);
+            } else if (0.5 < d && d < 0.75) {
+                this.setLocation(this.getX() + 1, this.getY());
+            } else {
+                this.setLocation(this.getX() - 1, this.getY());
+            }
+        }
+
+        double f = Math.random();
+        if (f < 0.3) {
+            turnAround();
+        }
+
     }
 }
