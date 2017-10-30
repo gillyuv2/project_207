dedb1b86b674c91a8b4578b092cd4f94b934c9ad
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2017-10-30 05:45:53 -0400

    Added the changes under the step 25 for the changes made in the FarmMethods.java.

--- a2/src/farmyard/FarmMethods.java
+++ a2/src/farmyard/FarmMethods.java
@@ -4,51 +4,60 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public abstract class FarmMethods {
-    private String appearance;
 
-    private int x;
-    private Color color;
-    private int y;
+  private String appearance;
+  private int x;
+  private Color color;
+  private int y;
+  private GraphicsContext g;
 
-    public FarmMethods(){
-    }
+  public FarmMethods() {}
 
-    public void setLocation(int x, int y){
+  void setLocation(int x, int y) {
+
+    if (x >= 48) {
+      this.x = 48;
+    } else if (x < 0) {
+      this.x = 1;
+    }else{
         this.x = x;
+    }
+    if (y >= 640/6) {
+      this.y = (640/6) -1 ;
+    } else if (y < 0) {
+      this.y = 1;
+    } else{
         this.y = y;
     }
+  }
 
-    int getX(){
-        return x;
-    }
+  int getX() {
+    return x;
+  }
 
-    int getY(){
-        return y;
-    }
+  int getY() {
+    return y;
+  }
 
-    void setColor(Color newColor){
-        this.color = newColor;
-    }
-    Color getColor(){
-        return this.color;
-    }
+  void setColor(Color newColor) {
+    this.color = newColor;
+  }
 
-    void drawString(GraphicsContext g, String s, int x, int y) {
-        g.setFill(this.color);
-        g.fillText(s, x * 10, y * 6);
-    }
+  void drawString(GraphicsContext g, String s, int x, int y) {
+    g.setFill(this.color);
+    g.fillText(s, x*10 , y*6 );
+  }
 
-    public void draw(GraphicsContext g) {
+  public void draw(GraphicsContext g) {
 
-        drawString(g, appearance, x, y);
-    }
+    drawString(g, appearance, x, y);
+  }
 
-    public String getAppearance(){
-        return appearance;
-    }
+  String getAppearance() {
+    return appearance;
+  }
 
-    void setAppearance(String newAppearance){
-        this.appearance = newAppearance;
-    }
-
+  void setAppearance(String newAppearance) {
+    this.appearance = newAppearance;
+  }
 }
