6fad39ff40e92a78fa6b199d1ae0a133c8b52047
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2017-10-30 05:26:12 -0400

    Added the cahanges in Wind.java and changes are noted in notes.txt in Step 22.

--- a2/src/farmyard/Wind.java
+++ a2/src/farmyard/Wind.java
@@ -1,12 +1,10 @@
 package farmyard;
 
-/** If the Wind is not blowing it is equally likely to move in one direction and has 10% chance
- * to remain still.
- * If wind is moving horizontal/vertical it is 60% likely to remain in same direction, 30% likely to reverse direction
- * and 10% likely to go still.
- *
- **/
-
+/**
+ * If the Wind is not blowing it is equally likely to move in one direction and has 10% chance to
+ * remain still. If wind is moving horizontal/vertical it is 60% likely to remain in same direction,
+ * 30% likely to reverse direction and 10% likely to go still.
+ */
 public class Wind {
 
   // Direction selector take 1, -1 and 0 as value
@@ -39,32 +37,32 @@ public class Wind {
    *
    * @return int direction
    */
-  private static int windDirection(int direction){
-    // If the wind is still then assign the direction to wind according to the probablity.
-    if (direction == 0){
-      if (Math.random() < 0.45){
+  private static int windDirection(int direction) {
+    // If the wind is still then assign the direction to wind according to the probability.
+    if (direction == 0) {
+        double d = Math.random();
+      if (d < 0.010) {
         direction = -1;
         return direction;
-      } else if (Math.random() > 0.45){
+      } else if (d < 0.020 && d > 0.010) {
         direction = 1;
         return direction;
-      } else {
+      } else if (d > 0.02) {
         return direction;
       }
     }
     // If the wind is moving then change or retain direction or stops the wind.
     else {
-       if (Math.random() > 0.9){
-         direction = 0;
-         return direction;
-       }else if (Math.random() < 0.6){
-         return direction;
-       }else{
-         direction = -direction;
-         return direction;
-       }
-    }
+      double d = Math.random();
+      if (d > 0.09) {
+        direction = 0;
+        return direction;
+      } else if (d < 0.045) {
+        return direction;
+      } else if (d < 0.09 && d > 0.045) {
+        direction = -direction;
+        return direction;
+      }
+    }return 0;
   }
 }
-
-
